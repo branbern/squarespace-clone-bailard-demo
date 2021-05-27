12a709ab18f889e972e64aca9b037c979e5cbdd5
brandon <bberning.web@gmail.com>
2021-05-27 09:59:31 -0400

    added the footer

--- src/Comps/Footer.jsx
+++ src/Comps/Footer.jsx
@@ -4,8 +4,9 @@ import '../Styles/Footer.scss';
 
 const Footer = () => {
     return (
-        <div className='info'>
-          
+        <div className='footer'>
+            <p>123 Demo Street, New York NY   |   (555) 555-5555   |   email@example.com</p>
+            <p>Made with my <span className='grey'>Skillz</span></p>
         </div>
     )
 }
